90f205fd8af53f3f70c5d06209899f554d53d946
Adrian <Adrian@Mac-Adrian.local>
2015-06-06 01:10:23 +0200

    adding tests

---
+++ src/test/CucumberTest.java
@@ -0,0 +1,12 @@
+
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by Adrian on 6/6/15.
+ */
+
+@RunWith(Cucumber.class)
+public class CucumberTest {
+
+}
---
+++ src/test/FilmsDefinitions.java
@@ -0,0 +1,37 @@
+import com.udl.softarch.randomfilms.config.SecurityConfig;
+import com.udl.softarch.randomfilms.repositories.FilmRepository;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+/**
+ * Created by Adrian on 6/6/15.
+ */
+@WebAppConfiguration
+@ContextConfiguration(classes = SecurityConfig.class)
+public class FilmsDefinitions {
+
+    @Autowired
+    WebApplicationContext webApp;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+    }
+
+    @Test
+    public void test_prueba(){
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
