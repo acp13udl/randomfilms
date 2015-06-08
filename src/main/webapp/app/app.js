b4d92e8cb427365324ba4d68d6cf584737d1e1eb
Adrian Carrera <adrian@MacBook-Pro-de-Adrian.local>
2015-06-08 21:35:39 +0200

    add angular module

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -1,36 +1,30 @@
 // principal configuration for randomfilms app
 (function(){
-    var app = angular.module("randomFilmsJS", ["random-films"]);
-
-    app.controller("InitialController",
-        ["$http", function($http) {
-            this.INITIALCONTROLLER_API = "../";
+    var app = angular.module('randomfilmsJS', ['random-films']);
+    console.log("paso inicio");
+    app.controller("InitialController", ["$http",
+        function($http) {
+            this.INITIALCONTROLLER_API = "https://randomfilms.herokuapp.com/";
             this.loading = false;
             var randomFilmsController = this;
-
+            console.log("initialcontroller");
             this.isLoading = function(){
                 return this.loading;
             };
 
             this.randomFilmsEmpty = function(){
-                return this.randomFilms === undefined;
-            }
+                console.log("entro?");
+                return undefined;
+            };
 
             this.listFilms = function(){
                 this.loading = true;
                 $http.get(this.INITIALCONTROLLER_API)
                     .success(function (data) {
                         randomFilmsController.films = data;
+                        console.log("Print");
                     });
             };
-
-            //this.addGreeting = function(){
-            //    $http.post(this.GREETINGS_API, this.newGreeting)
-            //        .then(function(){
-            //            greetingCtrl.newGreeting = {'date': Date.now()};
-            //            greetingCtrl.listGreetings();
-            //        });
-            //};
         }]);
 }());
 
