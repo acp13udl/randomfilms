12326fd353e00380950a225580370f2786986fef
Adrian Carrera <adrian@MacBook-Pro-de-Adrian.local>
2015-06-11 17:36:19 +0200

    adding film redirect

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -2,8 +2,8 @@
 (function(){
     var app = angular.module("randomfilmsJS", []);
     console.log("aqui");
-    app.controller("RandomFilmsController", ["$scope","$http",
-        function($scope,$http) {
+    app.controller("RandomFilmsController", ["$location","$scope","$http",
+        function($location,$scope,$http) {
             this.RANDOM_API = "../";
             var randomFilmsSuccess = false;
             var randomFilmsCrt = this;
@@ -11,6 +11,7 @@
                 'Accept': 'application/json'
             }
             }
+
 
             this.randomFilmsRequestSuccess = function(){
                 return randomFilmsSuccess;
@@ -44,13 +45,16 @@
 
             this.saveUrl = function(param){
                 $('#loading').show();
-                console.log("../save/"+param);
-                $http.get("../save/"+param,config)
+                console.log("../save/"+param+'&isAngular=0');
+                $http.get("../save"+param+'&isAngular=0',config)
                     .success(function (data) {
+                        console.log(JSON.stringify(data));
                         $('#loading').hide();
+                        window.location.assign("/app/films/film.html");
                     });
             }
 
             this.listFilms();
         }]);
-}());
+
+    }());
